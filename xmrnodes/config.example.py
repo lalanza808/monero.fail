157056ae9ab42b94c89e59f1d0bf37707cbeeb31
lza_menace <lza_menace@protonmail.com>
2020-10-21 22:17:17 -0700

    fix config example

--- xmrnodes/config.example.py
+++ xmrnodes/config.example.py
@@ -1,4 +1,4 @@
 import os
 
-SECRET_KEY = os.environ('SECRET_KEY', 'xxxx')
-DATA_DIR = os.environ('DATA_DIR', './data')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'xxxx')
+DATA_DIR = os.environ.get('DATA_DIR', './data')
