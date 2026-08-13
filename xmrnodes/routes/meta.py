77f57fe6786d393583b49229eb9e83e6e4adb070
lza_menace <lza_menace@protonmail.com>
2026-08-13 12:21:08 -0700

    add ipv6 filtering

--- xmrnodes/routes/meta.py
+++ xmrnodes/routes/meta.py
@@ -54,6 +54,8 @@ def index():
     elif node_type == "cors":
         nodes = nodes.where(Node.web_compatible == True)
         web_compatible = True
+    elif node_type == "ipv6":
+        nodes = nodes.where(Node.url.contains("["))
     else:
         nodes = nodes.where(Node.is_i2p == False, Node.is_tor == False)
 
